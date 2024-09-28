fb8190e4416eaadfada0b8f435aae9d0b111595b
Minebox260 <joellascoumes@gmail.com>
2024-09-28 20:20:12 +0200

    fix

--- commands/fishadmin.js
+++ commands/fishadmin.js
@@ -12,7 +12,6 @@ module.exports = {
 			subcommand
 				.setName('migrate')
 				.setDescription('Fetches all fish data from a channel and migrates it to the new database')
-				.setPermission('ADMINISTRATOR')
 				.addChannelOption(option =>
 					option.setName('channel')
 						.setDescription('The channel to fetch the data from')
